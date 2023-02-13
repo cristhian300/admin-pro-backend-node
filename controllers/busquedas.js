1f0c4662ea957333022fbaed87fd82f69e737941
Cristhian Curiñaupa Huaman <cristhian8730@outlook.com>
2023-02-13 16:47:31 -0500

    busqueda total con promesa

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -2,21 +2,38 @@
 
 
 const { response } = require("express")
+const  Usuario  = require('../models/usuario')
+const  Medicos  = require('../models/medico')
+const  Hospital  = require('../models/hospital')
 
-
-const getTodos = async  (req, res = response) => {
+const getTodos = async(req, res = response) => {
 
     const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, 'i');
+
+   /* const usuarios = await Usuario.find({ nombre: regex });
+    const medicos = await Medicos.find({ nombre: regex });
+    const hospital = await Hospital.find({ nombre: regex });
+*/
+ 
+   const [  usuarios,
+    medicos,
+    hospital]  =  await Promise.all([
+        await Usuario.find({ nombre: regex }),
+       await Medicos.find({ nombre: regex }),
+         await Hospital.find({ nombre: regex })
+    ])
+
 
     res.json({
-
         ok: true,
-        msg: 'todo ok',
-        busqueda
-    })
+        usuarios,
+        medicos,
+        hospital
+    });
 
 }
 
 
 
-module.exports = {getTodos}
+module.exports = { getTodos }
