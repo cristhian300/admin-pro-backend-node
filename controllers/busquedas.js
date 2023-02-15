65a68c221b374cfe0f5f04c9a9d74d9712379c6b
Cristhian Curiñaupa Huaman <cristhian8730@outlook.com>
2023-02-15 15:53:53 -0500

    busqueda por tablas

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -2,27 +2,27 @@
 
 
 const { response } = require("express")
-const  Usuario  = require('../models/usuario')
-const  Medicos  = require('../models/medico')
-const  Hospital  = require('../models/hospital')
+const Usuario = require('../models/usuario')
+const Medicos = require('../models/medico')
+const Hospital = require('../models/hospital')
 
-const getTodos = async(req, res = response) => {
+const getTodos = async (req, res = response) => {
 
     const busqueda = req.params.busqueda;
     const regex = new RegExp(busqueda, 'i');
 
-   /* const usuarios = await Usuario.find({ nombre: regex });
-    const medicos = await Medicos.find({ nombre: regex });
-    const hospital = await Hospital.find({ nombre: regex });
-*/
- 
-   const [  usuarios,
-    medicos,
-    hospital]  =  await Promise.all([
-        await Usuario.find({ nombre: regex }),
-       await Medicos.find({ nombre: regex }),
-         await Hospital.find({ nombre: regex })
-    ])
+    /* const usuarios = await Usuario.find({ nombre: regex });
+     const medicos = await Medicos.find({ nombre: regex });
+     const hospital = await Hospital.find({ nombre: regex });
+ */
+
+    const [usuarios,
+        medicos,
+        hospital] = await Promise.all([
+            await Usuario.find({ nombre: regex }),
+            await Medicos.find({ nombre: regex }),
+            await Hospital.find({ nombre: regex })
+        ])
 
 
     res.json({
@@ -36,4 +36,32 @@ const getTodos = async(req, res = response) => {
 
 
 
-module.exports = { getTodos }
+const getDocumentosColleccion =async (req, resp = response) => {
+
+    const busqueda = req.params.busqueda;
+    const tabla = req.params.tabla;
+
+    console.log('tabla',tabla);
+   const regExp = new RegExp(busqueda,'i')
+
+    switch (tabla) {
+        case 'usuarios':
+            const data = await Usuario.find({ nombre: regExp })
+
+            resp.json({
+                ok: true,
+                resultado:data
+            })
+            break;
+
+        default:
+           
+        resp.status(400).json({
+            ok:false,
+            msg:'La tabla tiene que ser usuarios/medicos/hospitales'
+        })
+    }
+
+}
+
+module.exports = { getTodos,getDocumentosColleccion }
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check} = require('express-validator');
-const { getTodos } = require("../controllers/busquedas");
+const { getTodos, getDocumentosColleccion } = require("../controllers/busquedas");
 const { validarJWT } = require("../middlewares/validar-jwt");
 /* api/todo */
 
@@ -12,6 +12,7 @@ router.get('/:busqueda',validarJWT,
 getTodos
 );
 
+router.get('/coleccion/:tabla/:busqueda',validarJWT,getDocumentosColleccion);
 
 
 module.exports = router;
