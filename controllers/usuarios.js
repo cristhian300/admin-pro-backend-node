28eb29b1dcfbd8c10287feeaa86a8c4f4ddba662
cristhian300 <56562843+cristhian300@users.noreply.github.com>
2023-03-09 11:23:12 -0500

    consumiendo api externo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,7 +2,7 @@ const Usuario = require('../models/usuario');
 const { response } = require('express')
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
-
+const axios = require('axios');
 
 const getUsuarios = async (req, res) => {
 
@@ -14,7 +14,7 @@ const getUsuarios = async (req, res) => {
 
     // const total = await Usuario.count();
 
-    const [usuarios, total ] = await Promise.all([
+    const [usuarios, total] = await Promise.all([
         Usuario.find({}, 'nombre email role google img')
             .skip(desde)
             .limit(10),
@@ -173,6 +173,75 @@ const borrarUsuario = async (req, res = response) => {
 
 }
 
+
+const usuarioByApi = async (req, res = response) => {
+
+
+
+    getTokenApi('cristhian8731', '123456').then(
+        x => console.log('resultado', x) 
+     )
+
+
+
+    // let url = `http://localhost:${process.env.PORT}/api/usuarios`;
+
+    // var config = {
+    //     method: 'get',
+    //     url: url,
+    //     headers: {
+    //         'x-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2M2E5ZjlmNWI0OTQ5NTZjZDc5NDAxMWUiLCJpYXQiOjE2NzgzMTI3NTYsImV4cCI6MTY3ODM1NTk1Nn0.kdDEQskBxs7dQCm97EcOPGRsq5jTliK0EKMvMT2IuYs'
+    //     },
+
+    //     data: null
+    // };
+
+    //   axios(config)
+    //     .then(function (response) {
+    //         //  console.log(JSON.stringify(response.data));
+    //         const { nombre, email } = response.data.usuarios[0]
+
+    //         const dataRecover = { nombre, email }
+    //         console.log('recuperado', dataRecover);
+    //         res.send({ recuperado: dataRecover })
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+
+}
+
+
+const getTokenApi = (email, password) => {
+    var data = JSON.stringify({
+        "email": "cristhian8731@gmail.com",
+        "password": "123456"
+    });
+
+    var config = {
+        method: 'post',
+        url: 'http://localhost:3001/api/login',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    return axios(config)
+        .then(function (response) {
+            console.log('api inicial token', JSON.stringify(response.data));
+
+            return response.data
+
+        })
+        .catch(function (error) {
+            console.log(error);
+
+        });
+
+}
+
+
 module.exports = {
-    getUsuarios, creandoUsuarios, actualizarUsuarios, borrarUsuario
+    getUsuarios, creandoUsuarios, actualizarUsuarios, borrarUsuario, usuarioByApi
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,15 +3,18 @@
 
  */
 const { Router } = require('express');
-const { getUsuarios, creandoUsuarios, actualizarUsuarios, borrarUsuario } = require('../controllers/usuarios')
+const { getUsuarios, creandoUsuarios, actualizarUsuarios, borrarUsuario, usuarioByApi } = require('../controllers/usuarios')
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 
 const router = Router();
+router.get('/byapi', usuarioByApi);
 
-router.get('/', validarJWT, getUsuarios);
+router.get('/',
+    validarJWT,
+    getUsuarios);
 
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
